7ee7b2e9210c717dcca979e144a97a8eff490167
heto1023 <heto1023@hs-karlsruhe.de>
2020-09-06 18:14:51 +0200

    Add script for convenient execution of the container passing in and out the videos

---
+++ docker-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 1. argument: path to a video directory on the host.
+# 2. argument: filename of the video to be processed as found in the path specified by the first argument.
+#
+# Returns: the output video placed in the video directory specified above.
+
+CONTAINER="trtpose-activity-demo:1"
+
+docker run --runtime nvidia -v $1:/videos $CONTAINER /bin/bash -c "cd IW276WS20-P12/src && python3 demo.py --video_path /videos/ --video_filename $2"
