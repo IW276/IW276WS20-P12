b9be7c440ef66c1576f899d61b66e0304af5191e
heto1023 <heto1023@hs-karlsruhe.de>
2020-09-06 15:34:00 +0200

    Add container tag

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,5 @@
 
 cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
-docker image build -f Dockerfile .
+
+docker image build -f Dockerfile -t trtpose-activity-demo:1 .
