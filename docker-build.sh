68225aca9c76f41caa7a901314b9d4d5483c8913
heto1023 <heto1023@hs-karlsruhe.de>
2020-09-06 18:11:31 +0200

    Clone repo to have the datasets and demo included in the container

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
+# pass-through opencv from host so we don't have to deal with
+# a time consuming compilation in the container
 cp /etc/apt/sources.list.d/nvidia-l4t-apt-source.list .
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc .
 
 docker image build -f Dockerfile -t trtpose-activity-demo:1 .
+
+# clean up after ourselves
+rm nvidia-l4t-apt-source.list
+rm jetson-ota-public.asc
+
