311408c31e30a1f49ad16dbfa01f1b85a212f7cf
heto1023 <heto1023@hs-karlsruhe.de>
2020-10-13 21:37:57 +0200

    Adjust docker scripts

--- docker-run-training.sh
+++ docker-run-training.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# 1. argument: path to a video directory on the host.
-# 2. argument: path to the training_data_dir
+# 1. argument: path to the training-data directory where the sample data can be found.
+# 2. argument: path to the model directory the resulting pre-trained model .pth will be placed in.
 #
-# Returns: the output video placed in the video directory specified above.
+# Returns: the pre-trained model.
 
 CONTAINER="trtpose-activity-demo:1"
 
-docker run --runtime nvidia -v $1:/videos -v $2:/training_data_dir $CONTAINER /bin/bash -c "cd IW276WS20-P12/src && python3 train_1_download_videos.py --video_dir /videos/ && python3 train_2_generate_annotations.py /videos/ /IW276WS20-P12/datasets/MPII_youtube_offline.csv --training_data_dir /training_data_dir --drop_dirty_pose"
+docker run --runtime nvidia  -v $1:/training_data_dir -v $2:/model_dir $CONTAINER /bin/bash -c "cd IW276WS20-P12/src && python3 train_3_train_model.py --train_dir /training_data_dir/ --model_dir /model_dir/"
 
 echo "Resulting training data can be found in $2"
