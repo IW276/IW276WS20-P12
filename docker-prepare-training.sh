4104d418adc4d5ca531274891d8763f1d9a5388d
ruka1011 <ruka1011@hs-karlsruhe.de>
2020-10-14 17:45:46 +0200

    fixed docker run command in docker-prepare-training.sh

--- docker-prepare-training.sh
+++ docker-prepare-training.sh
@@ -7,6 +7,6 @@
 
 CONTAINER="trtpose-activity-demo:1"
 
-docker run --runtime nvidia -v $1:/videos -v $2:/training_data_dir $CONTAINER /bin/bash -c "cd IW276WS20-P12/src && python3 train_1_download_videos.py --video_dir /videos/ && python3 train_2_generate_annotations.py /videos/ /IW276WS20-P12/datasets/MPII_youtube_offline.csv --training_data_dir /training_data_dir --drop_dirty_pose"
+docker run --runtime nvidia -v $1:/videos -v $2:/training_data_dir $CONTAINER /bin/bash -c "cd IW276WS20-P12/src && python3 train_1_download_videos.py  /videos/ && python3 train_2_generate_annotations.py /videos/ /IW276WS20-P12/datasets/MPII_youtube_offline.csv --training_data_dir /training_data_dir --drop_dirty_pose"
 
 echo "Resulting training data can be found in $2"
